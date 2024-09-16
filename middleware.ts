90b0f73bb6d4925f65a18f7eddb98f2feb575d62
FAIYAZ ULLAH <faiyazullah54@gmail.com>
2024-09-16 13:22:32 +0530

    authMiddleware issue find

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
   publicRoutes: ["/", "/api/webhooks/clerk"],
+  secretKey: process.env.CLERK_SECRET_KEY
 });
 export const config = {
   matcher: [
